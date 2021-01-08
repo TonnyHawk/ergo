3a2b401de050267c22c4d61a974ad09b60c8659e
TonnyHawk <anton.veremko@gmail.com>
2021-01-08 20:35:46 +0200

    Update nav.js

--- #src/styles/module/nav/nav.js
+++ #src/styles/module/nav/nav.js
@@ -1,43 +1,50 @@
 $(document).ready(function(){
-   if($(window).width() > 992){ // works only on pc screens
-
-      $("#topmenu .nav__toggler").click(function(){
-         $("#topmenu").toggleClass('nav_is-open');
-      });
-
-      $("#topmenu .nav__expand-btn").click(function(){
-         $("#topmenu .nav__expand-menu").toggleClass('show');
-      });
-
-      const menuItemHeight = 17.6;
-      function transportLastItem(){ // transports last menu-item to expand menu
-         lastMenuItem = $(".nav__expand-btn").prev().clone().removeClass().addClass('nav__menu-item_expand');
-         $(".nav__expand-btn").prev().remove();
-         $(".nav__expand-menu").append(lastMenuItem);
-      }
-
-      function checkForTwoLinesItem(){
+   function Nav(id){
+      this.id = '#'+id;
+      this.itemHeight = 17.6;
+      this.transportLastItem = ()=>{ // transports last menu-item to expand menu
+         let lastMenuItem = $(this.id+" .nav__expand-btn").prev().clone().removeClass().addClass('nav__menu-item_expand');
+         $(this.id+" .nav__expand-btn").prev().remove();
+         $(this.id+" .nav__expand-menu").append(lastMenuItem);
+      };
+      this.checkForTwoLinesItem = ()=>{
          let result = false;
-         $("#topmenu .nav__menu-item").each(function(){ // checks if there is any menu item that breaks on two lines
-            if($(this).height() > menuItemHeight){
+         let itemHeight = this.itemHeight;
+         $(this.id+" .nav__menu-item").each(function(){ // checks if there is any menu item that breaks on two lines
+            if($(this).height() > itemHeight){
                result = true;
                return true;
             }
          });
 
          return result;
-      }
+      };
+      this.adaptize = () =>{
+         let isMenuBroken = this.checkForTwoLinesItem();
+         if(isMenuBroken){
+            $(this.id+" .nav__expand-btn").addClass('show');
+         }
+   
+         while(isMenuBroken){
+            this.transportLastItem();
+            isMenuBroken = this.checkForTwoLinesItem();
+         }
+      };
 
-      let isMenuBroken = checkForTwoLinesItem();
-      if(isMenuBroken){
-         $(".nav__expand-btn").addClass('show');
-      }
+      // adding a click functionality to buttons
+      $(this.id+" .nav__toggler").click(function(){
+         $(this.id).toggleClass('nav_is-open');
+      });
 
-      while(isMenuBroken){
-         transportLastItem();
-         isMenuBroken = checkForTwoLinesItem();
-      }
+      $(this.id+" .nav__expand-btn").click(function(){
+         $(this.id+" .nav__expand-menu").toggleClass('show');
+      });
+   }
 
+   if($(window).width() > 992){ // works only on pc screens
+
+      let topmenu = new Nav('topmenu');
+      topmenu.adaptize();
    }
 
 });
